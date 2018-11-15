7a0d8692b169a9a8f4dcba63e1cd2d01c83f091e
Meolax <37709565+Meolax@users.noreply.github.com>
2018-11-15 13:32:51 +0300

    Clone FCFS

---
+++ Models/Executer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FCFS.Models;
+namespace FCFS.Models
+{
+    class Executer
+    {
+        public List<Proces> proceses = new List<Proces>();
+        public double AvgWaitTime
+        {
+            get
+            {
+                double AvgWait = 0;
+                foreach (Proces proc in proceses)
+                {
+                    AvgWait += proc.WaitTime;
+                }
+                return AvgWait / proceses.Count;
+            }
+        }
+        public double AvgExectTime
+        {
+            get
+            {
+                double AvgExect = 0;
+                foreach (Proces proc in proceses)
+                {
+                    AvgExect += proc.Te + proc.WaitTime;
+                }
+                return AvgExect / proceses.Count;
+            }
+        }
+        public double TimeExect { get; set; } = 0;
+        private double CurrentTime = 0;
+        enum Status
+        {
+            Ready,
+            Executing
+        }
+        Status status
+        {
+            get
+            {
+                return TimeExect <= CurrentTime ? Status.Ready : Status.Executing;
+            }
+        }
+
+
+        #region Executing
+        public Executer (List<Proces> proceses)
+        {
+            this.proceses = proceses;
+            proceses.Sort(new ProcComarer());
+        }
+
+        public void Execute ()
+        {
+            int completeProceses = 0;
+            while (completeProceses != proceses.Count)
+            {
+                if (status == Status.Ready && CurrentTime >= proceses[completeProceses].Tc)
+                {
+                    ExecuteProces(proceses[completeProceses]);
+                    completeProceses++;
+                }
+                else
+                    CurrentTime++;
+            }
+        }
+
+        private void ExecuteProces (Proces proces)
+        {
+            proces.WaitTime = CurrentTime - proces.Tc;
+            proces.ExectTime = CurrentTime;
+            TimeExect = CurrentTime + proces.Te;
+        }
+        #endregion
+    }
+}
---
+++ Models/Proceses.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCFS.Models
+{
+    public class Proces
+    {
+        private readonly static string ArgumentError = "Argument error!";
+
+        public int ID { get; }
+        public double Tc { get; }
+
+        public double Te { get; }
+
+        public static List<double> TcList = new List<double>();
+
+        public double WaitTime { get; set; }
+
+        public double ExectTime { get; set; }
+
+        public Proces (int ID, double Tc, double Te)
+        {
+            if (Tc < 0 || Te <= 0 || TcList.Contains(Tc))
+            {
+                throw new Exception($"public Proces (int {ID}, double {Tc}, double {Te}) -> "+ArgumentError);
+            }
+            this.ID = ID;
+            this.Tc = Tc+1;
+            TcList.Add(Tc);
+            this.Te = Te;
+        }        
+    }
+
+    public class ProcComarer: IComparer<Proces>
+    {
+        public int Compare (Proces x, Proces y)
+        {
+            return x.Tc > y.Tc ? 1 : x.Tc < y.Tc ? -1 : 0;
+        }
+    }
+}
